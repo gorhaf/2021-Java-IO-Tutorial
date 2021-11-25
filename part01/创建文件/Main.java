3829a1ed0f5a726a292144559d6e6fcfed041078
Gorhaf <gorhaf@163.com>
2021-11-25 22:02:37 +0800

    01：创建、删除、重命名文件目录
    看动画，学JavaIO教程01：创建、删除、重命名文件目录

---
+++ part01/创建文件/Main.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author 【B站】人人都是程序员
+ * @author 【掘金】人人都是程序员
+ * @author 【CSDN】人人都是程序员
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【官方网站】www.gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * @author 【个人微信号】gorhafapp
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 创建File对象
+        File file = new File("/Users/admin/Downloads/人人都是程序员.txt");
+        // 当文件不存在时
+        if (!file.exists()) {
+            try {
+                // 创建新文件
+                boolean result = file.createNewFile();
+                // 输出创建结果
+                System.out.println(result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ part01/递归删除/Main.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.io.File;
+
+/**
+ * @author 【B站】人人都是程序员
+ * @author 【掘金】人人都是程序员
+ * @author 【CSDN】人人都是程序员
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【官方网站】www.gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * @author 【个人微信号】gorhafapp
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 创建File对象
+        File file = new File("/Users/admin/Downloads/看动画学编程");
+        // 递归删除
+        boolean result = recursionDelete(file);
+        // 输出删除结果
+        System.out.println(result);
+    }
+
+    /**
+     * 递归删除
+     *
+     * @param file 文件或目录
+     * @return 删除成功返回true，否则返回false
+     */
+    public static boolean recursionDelete(File file) {
+        // 当为目录时
+        if (file.isDirectory() && file.exists()) {
+            // 列出目录下所以文件（或目录）
+            File[] files = file.listFiles();
+            // 当数组不为空时
+            if (files != null) {
+                // 遍历数组
+                for (File f : files) {
+                    // 递归删除文件（或目录）
+                    recursionDelete(f);
+                }
+            }
+        }
+        // 删除文件（或目录）
+        return file.delete();
+    }
+}
