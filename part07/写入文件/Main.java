5d0f10c85ee211b9ca2ab012379fed319de99785
Gorhaf <gorhaf@163.com>
2021-12-13 13:48:28 +0800

    07：字节输出流 OutputStream
    看动画，学JavaIO教程07：字节输出流 OutputStream

---
+++ part07/写入文件/Main.java
@@ -0,0 +1,68 @@
+package main;
+
+import java.io.*;
+
+/**
+ * @author 【B站】人人都是程序员
+ * @author 【掘金】人人都是程序员
+ * @author 【CSDN】人人都是程序员
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【官方网站】www.gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * @author 【个人微信号】gorhafapp
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 文件字节输出流
+        FileOutputStream out = null;
+        try {
+            // 创建File对象
+            File file = new File("/Users/admin/Downloads/人人都是程序员.txt");
+            // 当文件不存在时
+            if (!file.exists()) {
+                // 创建文件
+                file.createNewFile();
+            }
+            // 初始化文件字节输出流
+            out = new FileOutputStream(file);
+            // 要写入的字符串
+            String str = "abcdefgh";
+            // 转为字节数组
+            byte[] bytes = str.getBytes();
+            // 将数据输出到文件中
+            out.write(bytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 当输出流不为空时
+            if (out != null) {
+                try {
+                    // 关闭流
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
